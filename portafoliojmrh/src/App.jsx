d8b6f81fea676a88f7f8ccf713d6777eabe5b372
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-12-23 14:46:45 -0600

    [UPDATE] visible changes

--- portafoliojmrh/src/App.jsx
+++ portafoliojmrh/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import nightSkyImage from './assets/night-sky-Milky-Way-Galaxy.webp';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ backgroundImage: `url(${nightSkyImage})`, backgroundSize: 'cover' }} className="container-fluid vw-100 min-vh-100">
+      {/* NavBar */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">My Portfolio</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#about">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#skills">Skills</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className='text-white'>My Portfolio</h1>
+            <p className="lead text-white">Welcome to my portfolio. Here you can find information about my projects.</p>
+          </div>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col-md-12">
+            <h2 className= 'text-white'id="projects">My Projects</h2>
+            <div className="list-group">
+              <a href="https://project1.com" className="list-group-item list-group-item-action">
+                Project 1
+              </a>
+              <a href="https://project2.com" className="list-group-item list-group-item-action">
+                Project 2
+              </a>
+              {/* Add more project links as needed */}
+            </div>
+          </div>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col-md-12">
+            <h2 className='text-white' id="about">About Me</h2>
+            <p className='text-white'>
+              Insert a brief description about yourself here.
+            </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
