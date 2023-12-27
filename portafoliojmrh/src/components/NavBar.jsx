2740e28fe19856aeed923b2fb5fa922abbda73eb
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-12-26 18:43:11 -0600

    update

--- portafoliojmrh/src/components/NavBar.jsx
+++ portafoliojmrh/src/components/NavBar.jsx
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { Link } from 'react-router-dom';
-import homeIcon2 from "../../public/assets/homeIcon2.png"
+// import {homeIcon2} from "../../public/assets/homeIcon2.png"
 
 function NavBar() { 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img src={homeIcon2} alt="Home Icon" />
+          <img src={"../../public/assets/homeIcon2.png"} alt="Home Icon" />
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
