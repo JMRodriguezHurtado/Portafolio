b784f83a70ef13a45f0c2b5d3041170baa314453
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-12-26 18:54:58 -0600

    homeicon a public

--- portafoliojmrh/src/components/NavBar.jsx
+++ portafoliojmrh/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img src={"public/assets/homeIcon2.png"} alt="Home Icon" />
+          <img src={"public/homeIcon2.png"} alt="Home Icon" />
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
