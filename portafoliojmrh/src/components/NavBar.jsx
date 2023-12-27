0432565b34af809fe600521b30bbb86239e0fce0
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-12-26 19:09:58 -0600

    Update NavBar.jsx

--- portafoliojmrh/src/components/NavBar.jsx
+++ portafoliojmrh/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img src="/LOGO.png" alt="Home" />
+          <img src="/homeicon2.png" alt="Home" />
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
