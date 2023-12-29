d712eb01736c4d3f22724077e92028e4eea7c086
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-12-28 23:51:19 -0600

    Update NavBar.jsx

--- portafoliojmrh/src/components/NavBar.jsx
+++ portafoliojmrh/src/components/NavBar.jsx
@@ -1,18 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
+function NavBar() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
 
-function NavBar() { 
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <img src="/homeicon2.png" alt="Home" />
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
@@ -27,7 +40,7 @@ function NavBar() {
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default NavBar;
